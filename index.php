7178c36d9bfef85118ded41c9f7ee42a08a6e8b1
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2010-05-19 15:31:20 +0200

    Seperated between mobile and desktop version

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 
-/* Detect Mobile Safari */
+/* Detect Mobile Version */
 
 $user_agent = $_SERVER['HTTP_USER_AGENT'];
 
-if (strstr($user_agent, ' AppleWebKit/') && strstr($user_agent, ' Mobile/')) {
+if (
+  ((strstr($user_agent, ' AppleWebKit/') && strstr($user_agent, ' Mobile/')) || // it is an iPhone or iPod
+  preg_match('/android/i',$user_agent)) && // or an android
+  !preg_match('/ipad/i',$user_agent) // but not an iPad
+) {
     header('Location: iad.html');
 } else {
     require_once('desktop.html');
