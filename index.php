0c137c6d1eb46db250515514a8b35609de7f0237
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2010-05-19 15:18:39 +0200

    Renamed index.html to desktop.html

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $user_agent = $_SERVER['HTTP_USER_AGENT'];
 if (strstr($user_agent, ' AppleWebKit/') && strstr($user_agent, ' Mobile/')) {
     header('Location: iad.html');
 } else {
-    require_once('index.html');
+    require_once('desktop.html');
 }
 
 ?>
